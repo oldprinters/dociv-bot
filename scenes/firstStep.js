b70cab9292bb9b488dc7b730ff0b9052da60f229
Ivanych <zakaz@oldprinters.ru>
2024-07-03 12:23:33 +0300

    firstStep

--- scenes/firstStep.js
+++ scenes/firstStep.js
@@ -4,6 +4,10 @@ import { setCommands } from '../utils.js'
 
 const firstStep = new Scenes.BaseScene('FIRST_STEP')
 //--------------------------------------
+setupPatient.start( ctx => {
+    ctx.scene.reenter()
+})
+//--------------------------------------
 firstStep.enter(async ctx => {
     setCommands(ctx)
         if(ctx.session.role == "patient"){
